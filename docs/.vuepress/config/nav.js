932f9ed39791ac596590e1327e3230cc3d5adeb8
jiangxuejian <1137518450@qq.com>
2020-10-26 17:30:25 +0800

    d

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -4,11 +4,13 @@ module.exports = [
     text: '前端',
     link: '/web/',  //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
-      {text: '前端文章', items: [
+      {text: '前端', items: [
         {text: 'JavaScript', link: '/pages/8143cc480faf9a11/'}, // 这些link是在相应md文件定义的永久链接。另外，注意结尾是有斜杠的
         {text: 'Vue', link: '/pages/802a1ca6f7b71c59/'},
+        {text: 'HTML', link: '/pages/8309a5b876fc95e3/'},
+        {text: 'CSS', link: '/pages/0a83b083bdf257cb/'},
       ]},
-      {text: '学习笔记', items:[
+      {text: '前端笔记', items:[
         {text: '《JavaScript教程》笔记', link: '/note/javascript/'},
         {text: '《JavaScript高级程序设计》笔记', link: '/note/js/'},
         {text: '《ES6 教程》笔记', link: '/note/es6/'},
@@ -18,16 +20,9 @@ module.exports = [
       ]}
     ]
   },
+
   {
-    text: '页面', 
-    link: '/ui/',
-    items: [
-      {text: 'HTML', link: '/pages/8309a5b876fc95e3/'},
-      {text: 'CSS', link: '/pages/0a83b083bdf257cb/'},
-    ]
-  },
-  {
-    text: '技术',
+    text: '博客搭建',
     link: '/technology/',
     items: [
       {text: '技术文档', link: '/pages/9a7ee40fc232253e/'},
--- git deploy.sh
+++ git deploy.sh
@@ -1,3 +1,3 @@
 git add .
 git commit -m "d"
-git push 
+git push -f
