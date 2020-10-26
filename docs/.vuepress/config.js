5885b96c48c867702cacb925ac7e1f41c6958346
jiangxuejian <1137518450@qq.com>
2020-10-26 17:07:58 +0800

    d

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -100,6 +100,7 @@ module.exports = {
       color: '#11a8cd', // 爱心颜色，默认随机色
       excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
     }],
+    
 
     ['thirdparty-search', { // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
       thirdparty: [ // 可选，默认 []
